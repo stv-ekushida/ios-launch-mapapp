aa21e8be607707135c887a6e3ace4e7a6c1ed7e4
stv-ekushida <e.kushida@st-ventures.jp>
2020-02-06 09:26:23 +0900

    Initial Commit

---
+++ launch-mapapp-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  launch-mapapp-demo
+//
+//  Created by eiji kushida on 2020/02/06.
+//  Copyright © 2020 eiji kushida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ launch-mapapp-demoTests/launch_mapapp_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  launch_mapapp_demoTests.swift
+//  launch-mapapp-demoTests
+//
+//  Created by eiji kushida on 2020/02/06.
+//  Copyright © 2020 eiji kushida. All rights reserved.
+//
+
+import XCTest
+@testable import launch_mapapp_demo
+
+class launch_mapapp_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
