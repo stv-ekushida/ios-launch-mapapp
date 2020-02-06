547440942d0db258ba29f26c0196b51cc3f9b918
stv-ekushida <e.kushida@st-ventures.jp>
2020-02-06 09:36:59 +0900

    マップアプリを起動するサンプル

--- launch-mapapp-demo/ViewController.swift
+++ launch-mapapp-demo/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let latitude = "35.658581"
+    let longitude = "139.745433"
+    var urlString: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        launchMapApp()
+    }
+    
+    private func launchMapApp() {
+        if UIApplication.shared.canOpenURL(URL(string:"comgooglemaps://")!) {
+            
+            //https://developers.google.com/maps/documentation/urls/ios-urlscheme
+            //driving : driving / transit / bicycling / walking
+            urlString = "comgooglemaps://?daddr=\(latitude),\(longitude)&driving=driving&zoom=14"
+        }
+        else {
+            //https://developer.apple.com/library/archive/featuredarticles/iPhoneURLScheme_Reference/MapLinks/MapLinks.html
+            //dirflg ; d(by car) / w(by foot) / r(by public transit)
+            urlString = "http://maps.apple.com/?daddr=\(latitude),\(longitude)&dirflg=d"
+        }
+        if let url = URL(string: urlString) {
+            UIApplication.shared.open(url)
+        }
+    }
 }
 
